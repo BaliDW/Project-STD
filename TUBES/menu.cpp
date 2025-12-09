e64b14e9d9c57c26aca5147df9a467404152dc9c
Ahnfrmdhn <ahnafrmdhn12@gmail.com>
2025-12-09 10:25:17 +0700

    Tes menu

---
+++ TUBES/menu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void tampilkanMenu() {
+    // Membersihkan layar konsol (opsional, agar menu selalu terlihat bersih di atas)
+    // Di Windows bisa pakai system("cls"), di Linux/macOS pakai system("clear").
+    // Untuk contoh sederhana ini kita skip dulu agar tidak perlu library tambahan.
+    cout << "\n======================================" << endl;
+    cout << "           MENU UTAMA C++" << endl;
+    cout << "======================================" << endl;
+    cout << "  0. Defaults    - Tampilkan/Ubah default" << endl;
+    cout << "  1. View        - Lihat data" << endl;
+    cout << "  2. Edit        - Ubah data" << endl;
+    cout << "  3. Utility     - Menu utilitas" << endl;
+    cout << "  X. Exit        - Keluar dari program" << endl;
+    cout << "======================================" << endl;
+    cout << "Pilihan Anda (0-3, X): ";
+}
